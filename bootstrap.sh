ef2555fbaccc3d4fd799eb20c836e03be1163b76
Rawiri Blundell <rawiri.blundell@gmail.com>
2022-01-17 20:39:12 +1300

    Correct formatting for readlink_f(), add strlen()

--- bootstrap.sh
+++ bootstrap.sh
@@ -45,25 +45,51 @@ get_absolute_path() {
   unset -v _filename
 }
 
-readlink_f ()
-{
-    ( _count=0;
-    _target="${1:?No target specified}";
-    CDPATH='';
-    [ -e "${_target}" ] || return 1;
-    [ -L "${_target}" ] || return 1;
+readlink_f() {
+  (
+    _count=0
+    _target="${1:?No target specified}"
+    # Ensure that a customised CDPATH doesn't interfere
+    CDPATH=''
+
+    # Ensure that target actually exists and is actually a symlink
+    [ -e "${_target}" ] || return 1
+    [ -L "${_target}" ] || return 1
+
     while [ -L "${_target}" ]; do
-        _target="$(readlink "${_target}")";
-        _count=$(( _count + 1 ));
-        if [ "${_count}" -gt 20 ]; then
-            printf -- '%s\n' "readlink_f error: recursion limit reached" 1>&2;
-            return 1;
-        fi;
-    done;
-    cd "$(dirname "${_target}")" > /dev/null 2>&1 || return 1;
-    printf -- '%s\n' "${PWD%/}/${_target##*/}" )
+      _target="$(readlink "${_target}")"
+      _count=$(( _count + 1 ))
+      # This shouldn't be required, but just in case,
+      # we ensure that we don't get stuck in an infinite loop
+      if [ "${_count}" -gt 20 ]; then
+        printf -- '%s\n' "readlink_f error: recursion limit reached" >&2
+        return 1
+      fi
+    done
+    cd "$(dirname "${_target}")" >/dev/null 2>&1 || return 1
+    printf -- '%s\n' "${PWD%/}/${_target##*/}"
+  )
 }
 
+strlen() {
+  case "${1}" in
+    (-b|--bytes)
+      shift 1
+      LANG_orig="${LANG}"; LC_ALL_orig="${LC_ALL}"
+      LANG=C; LC_ALL=C; 
+      str="${*}"
+      printf -- '%d\n' "${#str}"
+      LANG="${LANG_orig}"; LC_ALL="${LC_ALL_orig}"
+    ;;
+    ('')
+      printf -- '%d\n' "0"
+    ;;
+    (*)
+      str="${*}"
+      printf -- '%d\n' "${#str}"
+    ;;
+  esac
+}
 
 # Function to work through a list of commands and/or files
 # and fail on any unmet requirements.  Example usage:
