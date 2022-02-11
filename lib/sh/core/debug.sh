62e3917eb870ebcb4bb2bdecc1f8a4b10f257836
Rawiri Blundell <rawiri.blundell@gmail.com>
2022-02-12 11:23:15 +1300

    Merged a number of ssl functions from ssl_audit - to be sanitised

--- lib/sh/core/debug.sh
+++ lib/sh/core/debug.sh
@@ -32,3 +32,50 @@ debug_err_handler() {
   done
   exit "${?}"
 }
+
+debug() {
+
+}
+
+# As above but with a pause 
+step() {
+
+}
+
+case "${1}" in
+  (-d|--debug)
+    set -xv
+    export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+    readonly debug_mode=true
+    trap 'set +x' EXIT
+  ;;
+esac
+
+# shellcheck disable=SC2183
+debug() {
+  [[ "${debug_mode}" != "true" ]] && return 0
+  : [DEBUG] "${*}"
+  : ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  read -n 1 -s -r -p "Press any key to continue"
+}
+
+echo a
+debug breakpoint one
+echo b
+debug breakpoint two
+
+---
+
+breakpoint(){
+    local REPLY
+    echo 'Breakpoint hit. [opaAxXq]'
+    while read -r -k1; do case $REPLY in
+        o) shopt -s; set -o ;;   # list options
+        p) declare -p | less ;;  # list parameters
+        a) declare -a ;;
+        A) declare -A ;;
+        x) set -x ;;             # toggle xtrace
+        X) set +x ;;
+        q) return ;;             # quit
+    esac; done
+}
