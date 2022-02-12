05b5e5f35e2839836db0b623bbf3b880cd087efa
Rawiri Blundell <rawiri.blundell@gmail.com>
2022-02-12 19:03:36 +1300

    Refactored view_certificate.sh

--- lib/sh/openssl/view_certificate.sh
+++ lib/sh/openssl/view_certificate.sh
@@ -22,134 +22,103 @@ if ! command -v openssl >/dev/null 2>&1; then
     exit 1
 fi
 
+# This function absorbs code from 
+# https://github.com/rawiriblundell/scripts/blob/master/ssl_audit
+# (c) 2019 Rawiri Blundell, Datacom Compute.  MIT License.
 view_certificate () {
-    _view_certificate_in="${1}"
+    _view_certificate_in="${2}"
 
     if (( "${#_view_certificate_in}" == 0 )); then
         printf -- 'view_certificate: %s\n' "No input file provided" >&2
         return 1
     fi
 
-    openssl x509 -text -noout -in "${_view_certificate_in}"
+    case "${1}" in
+        ([Aa]lgorithm)
+            _view_certificate_algorithm=$(
+                openssl x509 -text -noout -in "${_view_certificate_in}" | 
+                    awk -F ': ' '/Signature Algorithm/{print $2; exit}'
+            )
+
+            # Sometimes openssl isn't new enough to recognise a human friendly name
+            # and returns an oid instead.  We map these cases here, reference: http://oid-info.com
+            case "${_view_certificate_algorithm}" in
+                ('1.2.840.10045.4.3.1') _view_certificate_algorithm="ecdsa-with-SHA224" ;;
+                ('1.2.840.10045.4.3.2') _view_certificate_algorithm="ecdsa-with-SHA256" ;;
+                ('1.2.840.10045.4.3.3') _view_certificate_algorithm="ecdsa-with-SHA384" ;;
+                ('1.2.840.10045.4.3.4') _view_certificate_algorithm="ecdsa-with-SHA512" ;;
+                (*) : ;;
+            esac
+
+            printf -- '%s\n' "${_view_certificate_algorithm:-UNKNOWN}"
+            unset -v _view_certificate_algorithm
+        ;;
+        (CN|cn|[Cc]ommon[Nn]ame)
+            openssl x509 -in "${_view_certificate_in}" -subject -noout -nameopt multiline |
+                awk -F '= ' '/commonName/{print $2}' |
+                paste -sd "," -
+        ;;
+        ([Ee]mail)
+            openssl x509 -in "${_view_certificate_in}" -subject -noout -nameopt multiline |
+                awk -F '= ' '/emailAddress/{print $2}'
+        ;;
+        ([Ee]xpiry|[Ee]nd)
+            # First we test if it's a local file
+            if [ -r "${_view_certificate_in}" ]; then
+                openssl x509 -in "${_view_certificate_in}" -enddate -noout |
+                    sed -e "s/^notAfter=//" -e "s/GMT//" |
+                    awk '{printf("%s %02d %d %s\n", $1,$2,$4,$3)}'
+            # Otherwise, we assume a connection to a remote host
+            else
+                _view_certificate_host="${1}"
+                _view_certificate_port="${2:-443}"
+                echo | openssl s_client -showcerts -host "${_view_certificate_host}" -port "${_view_certificate_port}" 2>&1 \
+                    | openssl x509 -inform pem -noout -enddate \
+                    | cut -d "=" -f 2
+                unset -v _view_certificate_host _view_certificate_port
+            fi
+        ;;
+        ([Ff]ingerprint)
+            openssl x509 -in "${_view_certificate_in}" -fingerprint -noout
+        ;;
+        ([Ii]ssuer)
+            openssl x509 -in "${_view_certificate_in}" -issuer -noout |
+                sed -e "s/^issuer=//"
+        ;;
+        ([Mm]odulus)
+            openssl x509 -noout -modulus -in "${_view_certificate_in}" | shasum -a 256
+        ;;
+        ([Oo][Uu]|[Oo]rg|[Oo]rg[Nn]ame)
+            openssl x509 -in "${_view_certificate_in}" -subject -noout -nameopt multiline |
+                awk -F '= ' '/organizationalUnitName/{print $2}' |
+                paste -s -
+        ;;
+        ([Ss][Aa][Nn][Ss])
+            openssl x509 -in "${_view_certificate_in}" -text -noout |
+                grep "DNS:" |
+                trim |
+                paste -sd ' ' - |
+                grep .
+        ;;
+        ([Ss]erial)
+            openssl x509 -in "${_view_certificate_in}" -serial -noout |
+                sed -e "s/^serial=//"
+        ;;
+        ([Ss]tart)
+            openssl x509 -in "${_view_certificate_in}" -startdate -noout |
+                sed -e "s/^notBefore=//" -e "s/GMT//" |
+                awk '{printf("%s %02d %d %s\n", $1,$2,$4,$3)}'
+        ;;
+        ([Ss]tate)
+            case "$(openssl x509 -in "${_view_certificate_in}" -checkend "${2:-0}")" in
+                (*'not expire'*)  printf -- '%s\n' "OK" ;;
+                (*'will expire')  printf -- '%s\n' "EXPIRED" ;;
+            esac
+        ;;
+        (*)
+            openssl x509 -text -noout -in "${_view_certificate_in}"
+        ;;
+    esac
 
     unset -v _view_certificate_in
-}
-
-view_certificate_modulus() {
-    _view_certificate_modulus_in="${1}"
-
-    if (( "${#_view_certificate_modulus_in}" == 0 )); then
-        printf -- 'view_certificate_modulus: %s\n' "No input file provided" >&2
-        return 1
-    fi
-
-    openssl x509 -noout -modulus -in "${_view_certificate_modulus_in}" | shasum -a 256
-
-    unset -v _view_certificate_modulus_in
-}
-
-# The functions below are modified versions from 
-# https://github.com/rawiriblundell/scripts/blob/master/ssl_audit
-# (c) 2019 Rawiri Blundell, Datacom Compute.  MIT License.
-
-# Try to emit a certificate expiry date from openssl
-view_certificate_expiry() {
-    # First we test if it's a local file
-    if [ -r "${1}" ]; then
-        openssl x509 -in "${1}" -enddate -noout |
-            sed -e "s/^notAfter=//" -e "s/GMT//" |
-            awk '{printf("%s %02d %d %s\n", $1,$2,$4,$3)}'
-    # Otherwise, we assume a connection to a remote host
-    else
-        _view_certificate_host="${1}"
-        _view_certificate_port="${2:-443}"
-        echo | openssl s_client -showcerts -host "${_view_certificate_host}" -port "${_view_certificate_port}" 2>&1 \
-            | openssl x509 -inform pem -noout -enddate \
-            | cut -d "=" -f 2
-        unset -v _view_certificate_host _view_certificate_port
-    fi
-}
-
-# I know that the functions below result in multiple calls to openssl
-# This is computationally inefficient, but it's just how this script grew
-read_cert() {
-  openssl x509 -in "${1:?No Cert Defined}" -text -noout
-}
-
-read_cert_active() {
-  openssl x509 -in "${1:?No Cert Defined}" -startdate -noout |
-    sed -e "s/^notBefore=//" -e "s/GMT//" |
-    awk '{printf("%s %02d %d %s\n", $1,$2,$4,$3)}'
-}
-
-read_cert_algorithm() {
-  local cert_algorithm
-  cert_algorithm=$(read_cert "${1:?No Cert Defined}" | 
-    awk -F ': ' '/Signature Algorithm/{print $2; exit}')
-
-  # Sometimes openssl isn't new enough to recognise a human friendly name
-  # and returns an oid instead.  We map these cases here, 
-  # reference: http://oid-info.com
-  case "${cert_algorithm}" in
-    ('1.2.840.10045.4.3.1') cert_algorithm="ecdsa-with-SHA224" ;;
-    ('1.2.840.10045.4.3.2') cert_algorithm="ecdsa-with-SHA256" ;;
-    ('1.2.840.10045.4.3.3') cert_algorithm="ecdsa-with-SHA384" ;;
-    ('1.2.840.10045.4.3.4') cert_algorithm="ecdsa-with-SHA512" ;;
-    (*) : ;;
-  esac
-
-  printf -- '%s\n' "${cert_algorithm}"
-}
-
-read_cert_cn() {
-  openssl x509 -in "${1:?No Cert Defined}" -subject -noout -nameopt multiline |
-    awk -F '= ' '/commonName/{print $2}' |
-    paste -sd "," -
-}
-
-read_cert_email() {
-  openssl x509 -in "${1:?No Cert Defined}" -subject -noout -nameopt multiline |
-    awk -F '= ' '/emailAddress/{print $2}'
-}
-
-read_cert_expiry() {
-  openssl x509 -in "${1:?No Cert Defined}" -enddate -noout |
-    sed -e "s/^notAfter=//" -e "s/GMT//" |
-    awk '{printf("%s %02d %d %s\n", $1,$2,$4,$3)}'
-}
-
-read_cert_issuer() {
-  openssl x509 -in "${1:?No Cert Defined}" -issuer -noout |
-    sed -e "s/^issuer=//"
-}
-
-read_cert_ou_name() {
-  openssl x509 -in "${1:?No Cert Defined}" -subject -noout -nameopt multiline |
-    awk -F '= ' '/organizationalUnitName/{print $2}' |
-    paste -s -
-}
-
-# shellcheck disable=SC2119
-read_cert_sans() {
-  openssl x509 -in "${1:?No Cert Defined}" -text -noout |
-    grep "DNS:" |
-    trim |
-    paste -sd ' ' - |
-    grep .
-}
-
-read_cert_serial() {
-  openssl x509 -in "${1:?No Cert Defined}" -serial -noout |
-    sed -e "s/^serial=//"
-}
-
-read_cert_state() {
-  case "$(openssl x509 -in "${1:?No Cert Defined}" -checkend "${2:-0}")" in
-    (*'not expire'*)  printf -- '%s\n' "OK" ;;
-    (*'will expire')  printf -- '%s\n' "EXPIRED" ;;
-  esac
-}
-
-read_cert_fingerprint() {
-  openssl x509 -in "${1:?No Cert Defined}" -fingerprint -noout
 }
