9deb64d76e1bd80db9cd964caed15165cbbfecbf
Rawiri Blundell <rawiri.blundell@gmail.com>
2022-02-12 19:03:36 +1300

    Refactored view_certificate.sh

--- lib/sh/openssl/cert_split.sh
+++ lib/sh/openssl/cert_split.sh
@@ -19,3 +19,19 @@
 
 # TODO: Create a function that splits chained cert files
 # I likely have the code for this already - must check my archives...
+
+# If it's in a binary format, first convert to PEM
+# Then count the certs indicated by 'BEGIN CERTIFICATE'
+# If it's more than one, make a temporary directory and extract the certs into it
+# ... OR... build an array and put each cert into an element?
+
+# https://github.com/rawiriblundell/scripts/blob/master/ssl_audit
+# (c) 2019 Rawiri Blundell, Datacom Compute.  MIT License.
+
+#   cert_count=$(grep -E '\-BEGIN CERTIFICATE\-' "${cert_path}" | wc -l)
+
+# if (( cert_count > 1 )); then
+
+# extract_cert_bundle() {
+#   awk '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/{ if(/BEGIN/){a++}; out="cert"a".pem"; print >out}' "${1:?No Cert Defined}"
+# }
