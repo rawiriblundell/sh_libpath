8b46c002df3156d6c0ba25b020e0c9e763bbcde5
Rawiri Blundell <rawiri.blundell@gmail.com>
2022-02-17 00:19:07 +1300

    Add trim, chomp and chop functions, using retval approach

---
+++ lib/sh/text/str_ltrim.sh
@@ -0,0 +1,44 @@
+# shellcheck shell=ksh
+
+# Copyright 2022 Rawiri Blundell
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+# Provenance: https://github.com/rawiriblundell/sh_libpath
+# SPDX-License-Identifier: Apache-2.0
+
+str_ltrim() {
+  LC_CTYPE=C
+  _ltrim_str="${*}"
+  while true; do
+    ltrim_stdout="${_ltrim_str#[[:space:]]}"
+    [[ "${ltrim_stdout}" = "${_ltrim_str}" ]] && break
+    _ltrim_str="${ltrim_stdout}"
+  done
+  ltrim_rc="${?}"
+  unset -v _ltrim_str
+  export ltrim_stdout ltrim_rc
+}
+
+ltrim() {
+  LC_CTYPE=C
+  _ltrim_str="${*}"
+  while true; do
+    ltrim_stdout="${_ltrim_str#[[:space:]]}"
+    [[ "${ltrim_stdout}" = "${_ltrim_str}" ]] && break
+    _ltrim_str="${ltrim_stdout}"
+  done
+  ltrim_rc="${?}"
+  unset -v _ltrim_str
+  export ltrim_stdout ltrim_rc
+}
