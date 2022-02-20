af761b23a7b9b8447a5e3ce7eb9e3496cc4344d2
Rawiri Blundell <rawiri.blundell@gmail.com>
2022-02-21 00:05:35 +1300

    Add cidr.sh, str_padding.sh and temperature.sh

--- lib/sh/misc/larson.sh
+++ lib/sh/misc/larson.sh
@@ -1,77 +1,101 @@
-Your problem is that size can't really be done, *emphasis* is the closest text manipulation to what you're after.  I mean I *get*, conceptually, what you're trying to emulate (three bouncing dots that you've seen on plenty of mobile apps, right?) but the terminal by its very nature won't allow what you want.  Not without a LOT of work.
+# shellcheck shell=ksh
 
-Try something like this:
+# Copyright 2022 Rawiri Blundell
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+# Provenance: https://github.com/rawiriblundell/sh_libpath
+# SPDX-License-Identifier: Apache-2.0
 
-    for em in dim sgr0 bold sgr0 dim sgr0 bold sgr0 dim sgr0; do
-      tput "${em}"
-      printf -- '%s' '*'
-    done | paste -sd '' -
+# These functions setup a row of asterisks and then cycle through some text effects
+# This gives the 'look' of an animation of sorts - where each asterisk starts dim, 
+# then goes bright, then goes dim again, before moving on to its neighbour.
 
-If you're happy with the emphasis differences shown, then we can work with that. 
+# half_larson scans left to right before starting at the far most left
+# full_larson scans left to right and back again right to left
 
-I think you might have an easier time with a classic spinning wheel (I have code for that ready to go), or maybe something like a moving pipe symbol (think `--|----`, or `*|**`), or using a foreground/background colour combination via `tput` (e.g.: a green square cursor moving around)
+# These functions are named for the guy who developed this effect for the Cylons
+# in the original Battlestar Galactica, and for KITT in Knight Rider.
 
-   
-Ok, OP, you piqued my interest.  Here's my half-hearted attempt at it... I call it a "half-Larson", because it works its way left to right then starts at the left-most point again.  If it went left to right, then right to left, that would be a full Larson.  Named for the guy who developed the effect for the Cylons in Battlestar Galactica and KITT in Knight Rider.
+_larson_reset() {
+  tput sgr0     # Unset as many things that we've set as possible
+  tput cnorm    # Display the cursor again
+  printf '%s\n' ""
+  exit 0
+}
 
-    #!/bin/bash
-    
-    progWidth="${1:-3}"
-    sleepTime="0.2"
-    
-    resetTerm() {
-      tput sgr0     # Unset as many things that we've set as possible
-      tput cnorm    # Display the cursor again
-      printf '%s\n' ""
-      exit 0
-    }
-    
-    # Try to set things back the way they were, however we exit
-    trap resetTerm HUP INT QUIT TERM EXIT
-    
-    # GNU sleep can handle fractional seconds, non-GNU cannot
-    # so we default to 1 second resolution in that scenario
-    if ! sleep "${sleepTime}" &amp;&gt;/dev/null; then
-      sleepTime=1
-    fi
-    
-    tput sc                                    # Capture position
-    tput civis                                 # Hide cursor
-    tput dim                                   # Set base emphasis
-    printf '%*s' "${progWidth}" | tr ' ' "*"   # Setup base char width
-    while true; do                             # Infinite loop
-      tput rc                                  # Return to saved position
-      for ((i=0;i&lt;progWidth;i++)); do          # Iterate horizontally
-        for em in dim sgr0 bold sgr0 dim; do   # Emphasis sequence
-          printf -- '%s' "$(tput "${em}")*"    # Output emphasised char
-          sleep "${sleepTime}"                 # Pause for effect
-          tput cub1                            # Move left one char
-        done
-        tput cuf1                              # Move right one char
+# Try to set things back the way they were, however we exit
+trap _larson_reset HUP INT QUIT TERM EXIT
+
+half_larson() {
+  progWidth="${1:-3}"
+  sleepTime="0.2"
+ 
+  # GNU sleep can handle fractional seconds, non-GNU cannot
+  # so we default to 1 second resolution in that scenario
+  if ! sleep "${sleepTime}" >/dev/null 2>&1; then
+    sleepTime=1
+  fi
+  
+  tput sc                                    # Capture position
+  tput civis                                 # Hide cursor
+  tput dim                                   # Set base emphasis
+  printf '%*s' "${progWidth}" | tr ' ' "*"   # Setup base char width
+  while true; do                             # Infinite loop
+    tput rc                                  # Return to saved position
+    for ((i=0;i<progWidth;i++)); do          # Iterate horizontally
+      for em in dim sgr0 bold sgr0 dim; do   # Emphasis sequence
+        printf -- '%s' "$(tput "${em}")*"    # Output emphasised char
+        sleep "${sleepTime}"                 # Pause for effect
+        tput cub1                            # Move left one char
       done
+      tput cuf1                              # Move right one char
     done
-    
-A 0.1s sleepTime looks good too.
+  done
+}
 
-/edit: Full Larson... kinda...  same as before but with an extra loop:
-
-    while true; do                             # Infinite loop
-      tput rc                                  # Return to saved position
-      for ((i=0;i&lt;progWidth;i++)); do          # Iterate horizontally
-        for em in dim sgr0 bold sgr0 dim; do   # Emphasis sequence
-          printf -- '%s' "$(tput "${em}")*"    # Output emphasised char
-          sleep "${sleepTime}"                 # Pause for effect
-          tput cub1                            # Move left one char
-        done
-        tput cuf1                              # Move right one char
+full_larson() {
+  progWidth="${1:-3}"
+  sleepTime="0.2"
+ 
+  # GNU sleep can handle fractional seconds, non-GNU cannot
+  # so we default to 1 second resolution in that scenario
+  if ! sleep "${sleepTime}" >/dev/null 2>&1; then
+    sleepTime=1
+  fi
+  
+  tput sc                                    # Capture position
+  tput civis                                 # Hide cursor
+  tput dim                                   # Set base emphasis
+  printf '%*s' "${progWidth}" | tr ' ' "*"   # Setup base char width
+  while true; do                             # Infinite loop
+    tput rc                                  # Return to saved position
+    for ((i=0;i<progWidth;i++)); do          # Iterate horizontally
+      for em in dim sgr0 bold sgr0 dim; do   # Emphasis sequence
+        printf -- '%s' "$(tput "${em}")*"    # Output emphasised char
+        sleep "${sleepTime}"                 # Pause for effect
+        tput cub1                            # Move left one char
       done
-      tput cub1
-      for ((i=progWidth;i&gt;0;i--)); do
-        for em in dim sgr0 bold sgr0 dim; do
-          printf -- '%s' "$(tput "${em}")*"
-          sleep "${sleepTime}"
-          tput cub1
-        done
+      tput cuf1                              # Move right one char
+    done
+    tput cub1
+    for ((i=progWidth;i>0;i--)); do
+      for em in dim sgr0 bold sgr0 dim; do
+        printf -- '%s' "$(tput "${em}")*"
+        sleep "${sleepTime}"
         tput cub1
       done
+      tput cub1
     done
+  done
+}
