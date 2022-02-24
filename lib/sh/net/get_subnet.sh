7151db39139b091b06e9f3d34c36ac88a0fff8da
Rawiri Blundell <rawiri.blundell@gmail.com>
2022-02-24 23:10:41 +1300

    More function dumps from the archives

---
+++ lib/sh/net/get_subnet.sh
@@ -0,0 +1,37 @@
+# shellcheck shell=ksh
+
+# Copyright 2022 Rawiri Blundell
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+# Provenance: https://github.com/rawiriblundell/sh_libpath
+# SPDX-License-Identifier: Apache-2.0
+
+# Subnet Mask
+# Note: this is for the primary IP address only.
+# We try with 'ip', which requires CIDR conversion
+if iscommand ip; then
+  cidr=$(ip -o -f inet addr show | awk -F '/' '/scope global/{print $2}' | awk '{print $1}')
+  ipMask=$(Fn_cdr2mask "${cidr}")
+# If 'ifconfig' is present, we can try it like this:
+elif iscommand ifconfig; then
+  ipMask=$(ifconfig | grep -m 1 Mask | cut -d: -f4-)
+  # Again, if we're here, we're dealing with a different ifconfig output format
+  if [[ -z "${ipMask}" ]]; then
+    ipMask=$(ifconfig | awk '/netmask/{print $4; exit}')
+  fi
+fi
+
+# Solaris
+# Subnet Mask - requires gateway address
+#ipMask=$(netstat -nrv | grep "^$(echo "${gwAddr}" | cut -d"." -f1-3)" | awk '{print $2}')
