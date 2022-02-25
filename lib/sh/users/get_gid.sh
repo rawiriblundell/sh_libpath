295a720882e7247b635dcfc657258090984f1fea
Rawiri Blundell <rawiri.blundell@gmail.com>
2022-02-26 00:04:40 +1300

    Update multiple files

--- lib/sh/users/get_gid.sh
+++ lib/sh/users/get_gid.sh
@@ -1,10 +1,10 @@
 
 
-    func.PrintUser.GID() {
-      if awk -F : '{print $0}' /etc/passwd | grep ^"${username}" 1&gt;/dev/null; then
-        gid=$( grep ^"${username}" /etc/passwd | awk -F : '{print $4}' )
-        printf "%s\n" "${gid}"
-      else
-        func.Erruser
-      fi
-    }
+get_gid() {
+  if awk -F : '{print $0}' /etc/passwd | grep ^"${username}" >/dev/null 2>&1; then
+    gid=$(grep ^"${username}" /etc/passwd | awk -F : '{print $4}' )
+    printf "%s\n" "${gid}"
+  else
+    func.Erruser
+  fi
+}
