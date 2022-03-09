1839a6475dcf318b1f378c8d661cd69a324231fa
Rawiri Blundell <rawiri.blundell@gmail.com>
2022-03-09 23:08:32 +1300

    Add old system auditing code from the code attic

---
+++ lib/sh/sys/bios.sh
@@ -0,0 +1,47 @@
+# shellcheck shell=ksh
+
+# Copyright 2022 Rawiri Blundell
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+# Provenance: https://github.com/rawiriblundell/sh_libpath
+# SPDX-License-Identifier: Apache-2.0
+
+case "${OSSTR:-$(uname -s)}" in
+  ([lL]inux)
+    if iscommand dmidecode; then
+      sysBios=$(dmidecode | grep -m 1 -A 2 Vendor | awk -F ':' '{print $2}' | paste -sd '' - | trim)
+    fi
+  ;;
+  (SunOS|solaris)
+    # BIOS Version
+    if iscommand prtdiag; then
+      if prtdiag >/dev/null 2>&1; then
+        sysBios=$(prtdiag -v | grep -E '^OBP|^BIOS')
+      fi
+    elif smbios -t SMB_TYPE_BIOS >/dev/null 2>&1; then
+      sysBios=$( \
+        smbios -t SMB_TYPE_BIOS \
+        | grep -E 'Vendor|Version|Release' \
+        | awk -F ':' '{print $2}' \
+        | paste -sd ' ' - \
+        | trim)
+    else
+      sysBios=unknown
+    fi
+  ;;
+esac
+
+
+
+
---
+++ lib/sh/sys/mount.sh
@@ -0,0 +1,32 @@
+# shellcheck shell=ksh
+
+# Copyright 2022 Rawiri Blundell
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+# Provenance: https://github.com/rawiriblundell/sh_libpath
+# SPDX-License-Identifier: Apache-2.0
+
+if ! command -v mount >/dev/null 2>&1; then
+  printf -- 'get_mounts: %s\n' "This library requires 'mount', which was not found in PATH" >&2
+  exit 1
+fi
+
+get_mounts() {
+    #findmnt --real
+    if [[ $(mount | wc -l) -gt 0 ]]; then
+      mount
+    else
+      printInf "'mount' returned no output"
+    fi
+}
