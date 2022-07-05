dacb24ec8f2cb8e237c99694d623139f97aa573d
Rawiri Blundell <rawiri.blundell@gmail.com>
2022-07-05 22:57:33 +1200

    Resolve #8

--- lib/sh/text/pretty.sh
+++ lib/sh/text/pretty.sh
@@ -40,8 +40,33 @@ _pretty_select_random_color() {
 
 # Randomize text color for every fed line
 pretty () {
+  # Check if we are in an interactive shell or not
+  case "${-}" in
+    (*i*) : ;;
+    (*)   return 1 ;;
+  esac
+
+  # Check if we have any positional parameters
+  if (( "${#}" > 0 )); then
+    # If the first parameter is a readable file, process it line by line
+    if [[ -r "${1}" ]]; then
+      while read -r; do
+        # shellcheck disable=SC2046
+        printf -- "\033[38;5;%dm%s\033[0m\n" $(_pretty_select_random_color) "${REPLY}"
+      done < "${1}"
+      return 0
+    # Otherwise, process the positional parameter(s) as a string
+    else
+      # shellcheck disable=SC2046
+      printf -- "\033[38;5;%dm%s\033[0m\n" $(_pretty_select_random_color) "${*}"
+      return 0
+    fi
+  fi
+
+  # If we get to this point, then we're processing stdin line by line
   while read -r; do
     # shellcheck disable=SC2046
-    printf "\033[38;5;%dm%s\033[0m\n" $(_pretty_select_random_color) "${REPLY}"
+    printf -- "\033[38;5;%dm%s\033[0m\n" $(_pretty_select_random_color) "${REPLY}"
   done
+  return 0
 }
