41529bfb616bd8cded2001697e9e5f001027f436
Rawiri Blundell <rawiri.blundell@gmail.com>
2025-04-01 13:29:48 +1300

    lib/sh/text/rounded_box.sh: Update to improve formatting and multi-line handling

--- lib/sh/text/rounded_box.sh
+++ lib/sh/text/rounded_box.sh
@@ -18,20 +18,25 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # Print a given text within a rounded box
+# TODO: Accept heredoc inputs
+# TODO: Add coloured border handling e.g. red borders for warning messages
 rounded_box() {
     local u_left u_right b_left b_right h_bar v_bar h_width title content
-    u_left="\xe2\x95\xad"
-    u_right="\xe2\x95\xae"
-    b_left="\xe2\x95\xb0"
-    b_right="\xe2\x95\xaf"
-    h_bar="\xe2\x94\x80"
-    v_bar="\xe2\x94\x82"
-    h_width="78"
+    u_left="\xe2\x95\xad"   # upper left corner
+    u_right="\xe2\x95\xae"  # upper right corner
+    b_left="\xe2\x95\xb0"   # bottom left corner
+    b_right="\xe2\x95\xaf"  # bottom right corner
+    h_bar="\xe2\x94\x80"    # horizontal bar
+    v_bar="\xe2\x94\x82"    # vertical bar
+    h_width="78"            # default horizontal width
+
+    # Reset OPTIND
+    OPTIND=1
 
     while getopts ":ht:w:" flags; do
         case "${flags}" in
             (h)
-                printf -- '%s\n' "rounded_box (-w [width]) (-t [header]) [content]" >&2
+                printf -- '%s\n' "rounded_box (-t [title] -w [width in columns]) [content]" >&2
                 return 0
             ;;
             (t) title="${OPTARG}" ;;
@@ -41,28 +46,56 @@ rounded_box() {
     done
     shift "$(( OPTIND - 1 ))"
 
-    content="${*}"
+    # What remains after getopts is our content
+    # We store it this way to support multi-line input
+    content=$(printf -- '%s ' "${@}")
 
-    # Print our header
-    printf -- '%b' "${u_left}${h_bar}"
-    printf -- '%s' "${title}"
-    title_width=$(( h_width - ${#title} ))
-    for (( i=0; i<title_width; i++)); do
-        printf -- '%b' "${h_bar}"
-    done
+    # Print our top bar
+    printf -- '%b' "${u_left}"
+    # If the title is defined, then make space for it within the top bar
+    if [[ -n "${title}" ]]; then
+        # Calculate visual width of title (accounting for UTF-8)
+        title_visual_width=$(printf -- '%s' "${title}" | wc -m)
+        title_padding=$(( h_width - title_visual_width - 2 ))
+
+        printf -- '%b %s ' "${h_bar}" "${title}"
+        for (( i=0; i<title_padding; i++)); do
+            printf -- '%b' "${h_bar}"
+        done
+    # Otherwise, just print the full bar
+    else
+        for (( i=0; i<h_width; i++)); do
+            printf -- '%b' "${h_bar}"
+        done
+    fi
     printf -- '%b\n' "${h_bar}${u_right}"
 
     # Print our content
-    while read -r; do
-        printf -- '%b %s' "${v_bar}" "${REPLY}"
-        printf -- '%*s' "$(( h_width - ${#REPLY} ))"
-        printf -- ' %b\n' "${v_bar}"
-    done < <(fold -s -w "${h_width}" <<< ${content})
+    if [[ -n "${content}" ]]; then
+        # Replace literal "\n" with actual newlines
+        processed_content=$(printf -- '%s' "${content}" | sed 's/\\n/\n/g')
 
-    # Print our tail
+        # Process each line, including empty lines
+        while IFS= read -r line || [[ -n "${line}" ]]; do
+            # Wrap long lines with fold
+            while IFS= read -r folded_line; do
+                line_visual_width=$(printf -- '%s' "${folded_line}" | wc -m)
+                padding_width=$(( h_width - line_visual_width ))
+                printf -- '%b %s' "${v_bar}" "${folded_line}"
+                printf -- '%*s' "$padding_width"
+                printf -- ' %b\n' "${v_bar}"
+            done < <(printf '%s\n' "${line}" | fold -s -w "${h_width}")
+        done < <(printf -- '%s\n' "${processed_content}")
+    else
+        # Empty content - print one blank line
+        printf -- '%b %*s %b\n' "${v_bar}" "$h_width" "" "${v_bar}"
+    fi
+
+    # Print our bottom bar
     printf -- '%b' "${b_left}${h_bar}"
     for (( i=0; i<h_width; i++)); do
         printf -- '%b' "${h_bar}"
     done
     printf -- '%b\n' "${h_bar}${b_right}"
 }
+
