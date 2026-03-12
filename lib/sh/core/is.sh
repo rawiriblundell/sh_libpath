d72f2e787bfb498e21a21d1bac21e47085c5f410
Rawiri Blundell <rawiri.blundell@gmail.com>
2026-03-12 23:55:40 +1300

    lib/sh/core/is.sh: Massive refactor, add extra functions and sentinel check

--- lib/sh/core/is.sh
+++ lib/sh/core/is.sh
@@ -17,101 +17,188 @@
 # Provenance: https://github.com/rawiriblundell/sh_libpath
 # SPDX-License-Identifier: Apache-2.0
 
-is() {
-  [ "${1}" = "not" ] && shift 1; ! is "${@}"; return "${?}"
-  _condition="${1}"; _var_a="${2}"; _var_b="${3}"
-  case "${_condition}" in
-    (-[bcdefghLnprsStuwxz])
-      test "${_condition}" "${_var_a}";       return "${?}"
-    ;;
-    (command)       command -v "${_var_a}";   return "${?}" ;;
-    (file)          [ -f "${_var_a}" ];       return "${?}" ;;
-    (dir|directory) [ -d "${_var_a}" ];       return "${?}" ;;
-    (link|symlink)  [ -L "${_var_a}" ];       return "${?}" ;;
-    (exist|exists)  [ -e "${_var_a}" ];       return "${?}" ;;
-    (readable)      [ -r "${_var_a}" ];       return "${?}" ;;
-    (writeable)     [ -w "${_var_a}" ];       return "${?}" ;;
-    (executable)    [ -x "${_var_a}" ];       return "${?}" ;;
-    (unset)         [ -z "${_var_a+x}" ];     return "${?}" ;;
-    (set)
-      [ "${_var_a+x}" = "x" ] && [ "${#_var_a}" -gt "0" ]
-      return "${?}"
-    ;;
-    (empty|null)
-      [ "${_var_a+x}" = "x" ] && [ "${#_var_a}" -eq "0" ]
-      return "${?}"
-    ;;
-    (blank)
-      [ -z "${_var_a+x}" ] ||
-        {
-          [ "${_var_a+x}" = "x" ] && [ "${#_var_a}" -eq "0" ]
-        }
-      return "${?}"
-    ;;
-    (number|float)
-      printf -- '%f' "${1:-null}" >/dev/null 2>&1; return "${?}" ;;
-    (int|integer)
-      printf -- '%d' "${1:-null}" >/dev/null 2>&1; return "${?}" ;;
-    (substr|substring)
-      case "${_var_a}" in
-        (*"${_var_b}"*)  return 0 ;;
-        (''|*)           return 1 ;;
-      esac
-      [ -z "$1" ] || { [ -z "${2##*$1*}" ] && [ -n "$2" ];}; return "${?}" ;;
-    (true)
-      case "${_var_a}" in
-        (0|[tT][rR][uU][eE]|[yY][eE][sS]|[oO][nN])     return 0 ;;
-        (''|*)                                         return 1 ;;
-      esac
-    ;;
-    (false)
-      case "${_var_a}" in
-        (1|[fF][aA][lL][sS][eE]|[nN][oO]|[oO][fF][fF]) return 0 ;;
-        (''|*)                                         return 1 ;;
-      esac
-    ;;
-  esac > /dev/null 2>&1
-  return 1
+[ -n "${_SH_LOADED_core_is+x}" ] && return 0
+_SH_LOADED_core_is=1
+
+########## Paths
+path_exists() {
+  [ -e "${1:-$RANDOM}" ] >/dev/null 2>&1
+}
+
+path_is_file() {
+  [ -f "${1:-$RANDOM}" ] >/dev/null 2>&1
+}
+
+path_is_directory() {
+  [ -d "${1:-$RANDOM}" ] >/dev/null 2>&1
+}
+
+# TODO: path_is_hardlink()
+path_is_symlink() {
+  [ -L "${1:-$RANDOM}" ] >/dev/null 2>&1
+}
+
+path_is_readable() {
+  [ -r "${1:-$RANDOM}" ] >/dev/null 2>&1
+}
+
+path_is_writeable() {
+  [ -w "${1:-$RANDOM}" ] >/dev/null 2>&1
+}
+
+path_is_executable() {
+  [ -x "${1:-$RANDOM}" ] >/dev/null 2>&1
+}
+
+path_is_absolute() {
+  case "${1:-}" in
+    (/*) return 0 ;;
+    (*)  return 1 ;;
+  esac
+}
+
+path_is_relative() {
+  case "${1:-}" in
+    (/*) return 1 ;;
+    (*)  return 0 ;;
+  esac
+}
+
+path_is_empty_dir() {
+  [ -d "${1:-$RANDOM}" ] || return 1
+  [ -z "$(ls -A -- "${1}")" ]
+}
+
+########## Bools
+bool_is_false() {
+  case "${1:-null}" in
+    (1|[fF][aA][lL][sS][eE]|[nN][oO]|[oO][fF][fF]) return 0 ;;
+    (''|*)                                         return 1 ;;
+  esac
+}
+
+bool_is_true() {
+  case "${1:-null}" in
+    (0|[tT][rR][uU][eE]|[yY][eE][sS]|[oO][nN])     return 0 ;;
+    (''|*)                                         return 1 ;;
+  esac
+}
+
+bool_is_valid() {
+  case "${1:-null}" in
+    (0|1|[tT][rR][uU][eE]|[fF][aA][lL][sS][eE]|[yY][eE][sS]|[nN][oO]|[oO][nN]|[oO][fF][fF]) return 0 ;;
+    (''|*) return 1 ;;
+  esac
+}
+
+########## Numbers
+number_is_float() {
+  printf -- '%f' "${1:-null}" >/dev/null 2>&1
+}
+
+number_is_integer() {
+  # Leading "'" or '"' can be interpreted by %d as octal
+  # so we validate our inputs
+  case "${1:-null}" in
+    ("'"*|\"*) return 1 ;; 
+  esac
+  printf -- '%d' "${1:-null}" >/dev/null 2>&1
+}
+
+is_number() {
+  printf -- '%f' "${1:-null}" >/dev/null 2>&1
+}
+
+is_integer() {
+  number_is_integer "${1}"
+}
+
+is_float() {
+  number_is_float "${1}"
+}
+
+# Test if an integer is odd
+number_is_odd() {
+  (( (${1:?No number specified} % 2) != 0 ))
+}
+
+# Test if an integer is even
+number_is_even() {
+  (( (${1:?No number specified} % 2) == 0 ))
+}
+
+########## Vars
+# Test if a given value is a global var, local var (default) or array
+var_is_set() {
+  [ "${1+x}" = "x" ] && [ "${#1}" -gt "0" ]
+  return "${?}"
+}
+
+var_is_null() {
+  [ "${1+x}" = "x" ] && [ "${#1}" -eq "0" ]
+  return "${?}"
+}
+
+var_is_blank() {
+  [ -z "${1+x}" ] || { [ "${1+x}" = "x" ] && [ "${#1}" -eq "0" ]; }
+  return "${?}"
+}
+
+var_is_global() {
+  export -p | grep "declare -x ${1}=" >/dev/null 2>&1
+  return "${?}"
+}
+
+var_is_local() {
+  declare -p "${1}" 2>/dev/null | grep -- "-- ${1}=" >/dev/null 2>&1
+  return "${?}"
+}
+
+########## Misc
+is_array() {
+  declare -p "${1:-$RANDOM}" 2>/dev/null | grep -- "-a ${1:-$RANDOM}=" >/dev/null 2>&1
+}
+
+is_command() {
+  command -v "${1:-$RANDOM}" >/dev/null 2>&1
 }
 
 # Test if a given item is a function and emit a return code
 is_function() {
-  [[ $(type -t "${1:-grobblegobble}") = function ]]
+  typeset -f "${1:-grobblegobble}" >/dev/null 2>&1
 }
 
 # Are we within a directory that's tracked by git?
 is_gitdir() {
-  if [[ -e .git ]]; then
+  if [ -e .git ]; then
     return 0
   else
     git rev-parse --git-dir 2>&1 | grep -Eq '^.git|/.git'
   fi
 }
 
-# Test if a given value is a global var, local var (default) or array
-is_set() {
+is_substring() {
   case "${1}" in
-    (-a|-array)
-      declare -p "${2}" 2>/dev/null | grep -- "-a ${2}=" >/dev/null 2>&1
-      return "${?}"
-    ;;
-    (-g|-global)
-      export -p | grep "declare -x ${2}=" >/dev/null 2>&1
-      return "${?}"
-    ;;
-    (-h|--help|"")
-      printf -- '%s\n' "Function to test whether NAME is declared" \
-        "Usage: is_set [-a(rray)|-l(ocal var)|-g(lobal var)|-h(elp)] NAME" \
-        "If no option is supplied, NAME is tested as a local var"
-      return 0
-    ;;
-    (-l|-local)
-      declare -p "${2}" 2>/dev/null | grep -- "-- ${2}=" >/dev/null 2>&1
-      return "${?}"
-    ;;
-    (*)
-      declare -p "${1}" 2>/dev/null | grep -- "-- ${1}=" >/dev/null 2>&1
-      return "${?}"
-    ;;
+    (*"${2}"*)  return 0 ;;
+    (''|*)      return 1 ;;
   esac
+}
+
+# Are we running interactively?
+is_interactive() {
+  case "$-" in
+    (*i*) return 0 ;;
+    (*)   return 1 ;;
+  esac
+}
+
+# Are we running as root?
+is_root() {
+  (( ${EUID:-$(id -u)} == 0 ))
+}
+
+# Is the current script being sourced rather than executed?
+# Caveat: bash-only for now.  Needs more...
+is_sourced() {
+  [ "${BASH_SOURCE[0]}" != "${0}" ]
 }
